804b9e44eca3ab79d5bed4508ca63b133e655607
axel <axel.rl.404@gmail.com>
2023-10-01 10:41:56 +0700

    docs: limiter v2 example

---
+++ example/limit_v2/selector.go
@@ -0,0 +1,40 @@
+// This file contains function to help you switch limiter type easily.
+//
+// You can use this in your project if you want.
+//
+// You don't need to handle all limiter types, just handle
+// the ones you will use.
+package main
+
+import (
+	"errors"
+	"time"
+
+	limit "github.com/rl404/fairy/limit_v2"
+	"github.com/rl404/fairy/limit_v2/atomic"
+	"github.com/rl404/fairy/limit_v2/mutex"
+)
+
+// LimitType is type for rate limit.
+type LimitType int8
+
+// Available types for rate limit.
+const (
+	Mutex LimitType = iota
+	Atomic
+)
+
+// ErrInvalidLimitType is error for invalid rate limit type.
+var ErrInvalidLimitype = errors.New("invalid rate limit type")
+
+// New to create new rate limiter.
+func New(limitType LimitType, rate int, interval time.Duration) (limit.Limiter, error) {
+	switch limitType {
+	case Mutex:
+		return mutex.New(rate, interval), nil
+	case Atomic:
+		return atomic.New(rate, interval), nil
+	default:
+		return nil, ErrInvalidLimitype
+	}
+}
